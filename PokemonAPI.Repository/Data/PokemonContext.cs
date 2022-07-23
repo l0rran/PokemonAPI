2c2ce2e904e8a700fb1db5349196012acb8ff603
Wesley <wesleycct@gmail.com>
2022-07-23 12:35:45 -0300

    Added EntityFramework and created the PokemonContext

---
+++ PokemonAPI.Repository/Data/PokemonContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PokemonAPI.Repository.Models.Pokemon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonAPI.Repository.Data
+{
+    public class PokemonContext : DbContext
+    {
+
+        public PokemonContext(DbContextOptions<PokemonContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Pokemon> Pokemons { get; set; }
+
+        public DbSet<Status> Statuses { get; set; }
+
+        public DbSet<PokemonSpecies> Species { get; set; }
+
+        public DbSet<PokemonType> Types { get; set; }
+
+    }
+}
