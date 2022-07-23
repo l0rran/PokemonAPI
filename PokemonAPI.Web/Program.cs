6e24cf9e360365e09a4f644262c542caeafd47a8
Wesley <wesleycct@gmail.com>
2022-07-23 14:24:24 -0300

    Added EntityFramework and Repository reference to Web project and initilized EntityFramework

--- PokemonAPI.Web/Program.cs
+++ PokemonAPI.Web/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using PokemonAPI.Repository.Data;
+using PokemonAPI.Repository.Models.Pokemon;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<PokemonContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
