e2e4b5b72c30846fe3ff07ca59bf42f717e6b89e
Wesley <wesleycct@gmail.com>
2022-07-23 17:09:43 -0300

    Created Pokemon Service

---
+++ PokemonAPI.Repository/Services/IPokemonService.cs
@@ -0,0 +1,18 @@
+﻿using PokemonAPI.Repository.DTO.Pokemon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonAPI.Repository.Services
+{
+    public interface IPokemonService
+    {
+        Task<List<PokemonMinimalDTO>> GetAllAsync();
+
+        Task<List<PokemonMinimalDTO>> GetPaginatedAsync(int skip, int take);
+
+        Task<PokemonDTO> GetByIdAsync(int id);
+    }
+}
---
+++ PokemonAPI.Repository/Services/PokemonService.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PokemonAPI.Repository.Data;
+using PokemonAPI.Repository.DTO.Pokemon;
+using PokemonAPI.Repository.Models.Pokemon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonAPI.Repository.Services
+{
+    public class PokemonService : IPokemonService
+    {
+        private readonly PokemonContext _pokemonContext;
+
+        public PokemonService(PokemonContext pokemonContext)
+        {
+            _pokemonContext = pokemonContext;
+        }
+        public async Task<List<PokemonMinimalDTO>> GetAllAsync()
+        {
+            return await _pokemonContext.Pokemons.Select(p => new PokemonMinimalDTO()
+            {
+                Id = p.Id,
+                Name = p.Name,
+                Number = p.Number
+            }).ToListAsync();
+        }
+
+        public async Task<List<PokemonMinimalDTO>> GetPaginatedAsync(int skip, int take)
+        {
+            return await _pokemonContext.Pokemons.Skip(skip).Take(take).Select(p => new PokemonMinimalDTO()
+            {
+                Id = p.Id,
+                Name = p.Name,
+                Number = p.Number
+            }).ToListAsync();
+        }
+
+        public async Task<PokemonDTO> GetByIdAsync(int id)
+        {
+            Pokemon pokemon = await _pokemonContext.Pokemons.FindAsync(id);
+            if (pokemon == null)
+            {
+                return null;
+            }
+
+            return new PokemonDTO()
+            {
+                Id = pokemon.Id,
+                Description = pokemon.Description,
+                Evolutions = pokemon.Evolutions,
+                HeightInMeters = pokemon.HeightInMeters,
+                Locals = pokemon.Locals,
+                Name = pokemon.Name,
+                Number = pokemon.Number,
+                Species = pokemon.Species,
+                Stats = pokemon.Stats,
+                Types = pokemon.Types,
+                WeightInKg = pokemon.WeightInKg
+            };
+        }
+
+        
+    }
+}
